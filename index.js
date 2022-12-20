4ae1880ce59de93942f1bc7e9b632d2d71b029ae
Ioseb1999 <guruli.websmart@gmail.com>
2022-12-20 19:06:03 +0400

    configured twitter APP

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
 const express = require('express')
+const passport = require('passport')
+const Strategy = require('passport-twitter').Strategy
+const session = require('express-session')
+const cors = require('cors')
+
+passport.use(
+  new Strategy(
+    {
+      consumerKey: 'yV34MsRaX9DRCsjVwSlNLVTsI',
+      consumerSecret: 'dGCVUxzE66KLxTjFIRSnqFbcjW1TV1p6WC52cqrgKpXSRp4itn',
+      callbackURL: 'http://localhost:8080/twitter'
+    },
+    (token, tokenSecret, profile, callback) => {
+      return callback(null, profile)
+    }
+  )
+)
+
+passport.serializeUser((user, callback) => {
+  try {
+    callback(null, user)
+  } catch (error) {
+    return error
+  }
+})
+
+passport.deserializeUser((obj, callback) => {
+  try {
+    callback(null, obj)
+  } catch (error) {
+    return error
+  }
+})
 
 const app = express()
-
+app.use(
+  session({ secret: 'twitter2020!', resave: true, saveUninitialized: true })
+)
+app.use(cors())
 app.get('/', (req, res) => {
   res.send('Hello From Twitter + Nodejs App')
 })
